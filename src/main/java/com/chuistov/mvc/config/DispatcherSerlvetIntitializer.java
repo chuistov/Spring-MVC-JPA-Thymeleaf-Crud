dba0e774f395a341c310c978f1814faf9a24b3d4
chuistov <108229048+chuistov@users.noreply.github.com>
2022-12-25 23:36:48 +0300

    Cyrillic enabled.

--- src/main/java/com/chuistov/mvc/config/DispatcherSerlvetIntitializer.java
+++ src/main/java/com/chuistov/mvc/config/DispatcherSerlvetIntitializer.java
@@ -1,8 +1,10 @@
 package com.chuistov.mvc.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -23,14 +25,30 @@ public class DispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispa
         return new String[] {"/"};
     }
 
+//    @Override
+//    public void onStartup(ServletContext context) throws ServletException {
+//        super.onStartup(context);
+//        registerHiddenFieldFilter(context);
+//    }
+//
+//    private void registerHiddenFieldFilter(ServletContext context) {
+//        context.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+//                .addMappingForUrlPatterns(null ,true, "/*");
+//    }
+
     @Override
     public void onStartup(ServletContext context) throws ServletException {
         super.onStartup(context);
         registerHiddenFieldFilter(context);
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        FilterRegistration.Dynamic filterRegistration =
+                context.addFilter("characterEncodingFilter", characterEncodingFilter);
+        filterRegistration.addMappingForUrlPatterns(null, false, "/*");
     }
-
-    private void registerHiddenFieldFilter(ServletContext context) {
-        context.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
-                .addMappingForUrlPatterns(null ,true, "/*");
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
--- src/main/java/com/chuistov/mvc/config/SpringConfig.java
+++ src/main/java/com/chuistov/mvc/config/SpringConfig.java
@@ -46,6 +46,7 @@ public class SpringConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding("UTF-8");
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
